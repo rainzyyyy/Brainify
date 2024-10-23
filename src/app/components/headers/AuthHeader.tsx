1dcb714f19c6787f1922960425f7816ae041bfda
rainzyyyy <hew78pikrou@gmail.com>
2024-10-23 11:01:46 +0800

    fix: resolve hydration error

--- src/app/components/headers/AuthHeader.tsx
+++ src/app/components/headers/AuthHeader.tsx
@@ -4,22 +4,24 @@ import Link from "next/link";
 
 const AuthHeader: React.FC = () => {
   return (
-    <nav className='flex justify-between items-center border border-b-middleGrey bg-chineseBlack p-6'>
+    <nav className="flex justify-between items-center border border-b-middleGrey bg-chineseBlack p-6">
       <div>
-        <Image src='/assets/logos/brainify.png' alt="Brainify logo" width={180} height={50} />
+        <Image src="/assets/logos/brainify.png" alt="Brainify logo" width={180} height={50} />
       </div>
 
-      <div className='flex space-x-14 mx-20'>
-        <Link href="#"> {/* TODO: link a page */}
-          <a className='border border-middleGrey px-8 py-2 text-xl text-middleGrey rounded-lg hover:bg-middleGrey hover:text-babyPowder'>
+      <div className="flex space-x-14 mx-20">
+        <Link href="#">
+          {/* TODO: link a page */}
+          <div className="border border-middleGrey px-8 py-2 text-xl text-middleGrey rounded-lg hover:bg-middleGrey hover:text-babyPowder">
             Sign up
-          </a>
+          </div>
         </Link>
 
-        <Link href="#"> {/* TODO: link a page */}
-          <a className='text-xl text-middleGrey hover:text-babyPowder'>
+        <Link href="#">
+          {/* TODO: link a page */}
+          <div className="text-xl text-middleGrey hover:text-babyPowder">
             Log in
-          </a>
+          </div>
         </Link>
       </div>
     </nav>
